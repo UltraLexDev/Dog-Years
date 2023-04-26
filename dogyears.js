9013d0c5838d8af9d44ba3b94ae3d6be8d0899b7
UltraLexDev <130335826+UltraLexDev@users.noreply.github.com>
2023-04-26 16:06:31 -0700

    Create dogyears.js

---
+++ dogyears.js
@@ -0,0 +1,21 @@
+// My Age
+const myAge = 32;
+
+// Early Years
+var earlyYears = 2;
+console.log(earlyYears *= 10.5);
+
+// Later Years
+var laterYears = myAge - 2;
+
+// Later Years Calc
+console.log(laterYears *= 4);
+
+// My age in dog years
+var myAgeInDogYears = earlyYears + laterYears;
+
+// My name in lowercase
+var myName = ('ALEX'.toLowerCase());
+
+// Output
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
